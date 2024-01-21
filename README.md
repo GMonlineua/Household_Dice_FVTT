3f88d36333e54b75e2727b1cd2e85103bf696773
Vlad Dracula <gmonlineua@gmail.com>
2024-01-21 16:07:10 +0200

    version 1.0

---
+++ README.md
@@ -0,0 +1,22 @@
+# Household Dice Roller
+
+[![Foundry Shield 11]][Foundry URL]
+[![Version]][Version URL]
+[![GNU License]][GNU URL]
+
+### Installation Instructions
+
+To install a module, follow these instructions:
+
+1. Start FVTT and browse to the Game Modules tab in the Configuration and Setup menu
+2. Select the Install Module button and enter the following URL: https://raw.githubusercontent.com/GMonlineua/Household_Dice_FVTT/master/system.json
+3. Click Install and wait for installation to complete
+
+[Foundry Shield 11]: https://img.shields.io/badge/Foundry-11-informational?style=flat-square
+[Foundry URL]: https://foundryvtt.com
+
+[Version]: https://img.shields.io/badge/Version-1.0-orange?style=flat-square
+[Version URL]: https://github.com/GMonlineua/Foundry_Rogue
+
+[GNU License]: https://img.shields.io/badge/License-GNU-green?style=flat-square
+[GNU URL]: https://github.com/GMonlineua/Foundry_Rogue/blob/master/LICENSE.md
---
+++ household-dice.js
@@ -0,0 +1,145 @@
+class HouseholdDice {
+
+  static async Init(controls, html) {
+    const diceRollButton = $(`
+      <li class="scene-control hhd-scene-control" data-control="household-dice" title="Household Dice Roller">
+      <i class="fas fa-dice-d20"></i>
+      </li>
+    `);
+
+    const diceRollControls = $(`
+      <ol class="sub-controls app control-tools hhd-sub-controls">
+      <li id="HHDpopup" class="household-dice-popup control-tool"></li>
+      </ol>
+    `);
+
+    html.find(".main-controls").append(diceRollButton);
+    html.append(diceRollControls);
+    diceRollButton[0].addEventListener('click', ev => this.PopupSheet(ev, html));
+    this._createDiceTable(html);
+  }
+
+  static _createDiceTableHtml() {
+    let s = [];
+
+    s.push('<ul>');
+    for (let i = 2; i <= 9; ++i) {
+      s.push('<li data-dice-roll="', i, '">', i, '</li>');
+    }
+    s.push('</ul>');
+
+    return s.join('');
+  }
+
+  static async _createDiceTable(html) {
+    const tableContentsHtml = this._createDiceTableHtml();
+    const tableContents = $(tableContentsHtml);
+
+    html.find('.household-dice-popup ul').remove();
+    html.find('.household-dice-popup').append(tableContents);
+    html.find('.household-dice-popup li').click(ev => this._rollDice(ev, html));
+  }
+
+  static async _rollDice(event, html) {
+    const diceCount = event.target.dataset.diceRoll;
+    let formula = diceCount + "d6"
+    const roll = new Roll(formula);
+    await roll.evaluate({ async: true });
+
+    let rollResult = {
+      commonDice: roll.terms[0].results.map((result) => ({ value: result.result, used: false })),
+      basicSuccess: 0,
+      criticalSuccess: 0,
+      extremeSuccess: 0,
+      impossibleSuccess: false,
+      failure: false
+    };
+
+    // Count the occurrences of each dice value
+    const diceCounts = rollResult.commonDice.reduce((counts, die) => {
+        counts[die.value] = (counts[die.value] || 0) + 1;
+        return counts;
+    }, {});
+
+    // Check for success based on the counts
+    Object.values(diceCounts).forEach(count => {
+        if (count === 2) {
+            rollResult.basicSuccess++;
+        } else if (count === 3) {
+            rollResult.criticalSuccess++;
+        } else if (count === 4) {
+            rollResult.extremeSuccess++;
+        } else if (count === 5) {
+            rollResult.impossibleSuccess = true;
+        }
+    });
+
+    // Check for failure
+    if (
+        rollResult.basicSuccess === 0 &&
+        rollResult.criticalSuccess === 0 &&
+        rollResult.extremeSuccess === 0 &&
+        !rollResult.impossibleSuccess
+    ) {
+        rollResult.failure = true;
+    }
+
+    const chatMessage = await renderTemplate("modules/household-dice/templates/rollResult.hbs", rollResult);
+    const chatData = {
+      user: game.user._id,
+      content: chatMessage,
+      type: CONST.CHAT_MESSAGE_TYPES.ROLL,
+      sound: CONFIG.sounds.dice
+    };
+
+    ChatMessage.create(chatData);
+
+    this._close(event, html);
+  }
+
+  static async PopupSheet(event, html) {
+    if (html.find('.hhd-scene-control').hasClass('active')) {
+      this._close(event, html);
+    } else {
+      this._open(event, html);
+  }
+  }
+
+  static async _close(event, html) {
+    html.find('.hhd-scene-control').removeClass('active');
+    html.find('.hhd-sub-controls').removeClass('active');
+    html.find('.scene-control').first().addClass('active');
+
+    event.stopPropagation();
+  }
+
+  static async _open(event, html) {
+    this._createDiceTable(html);
+    html.find('.scene-control').removeClass('active');
+    html.find('.sub-controls').removeClass('active');
+    html.find('.hhd-scene-control').addClass('active');
+    html.find('.hhd-sub-controls').addClass('active');
+    event.stopPropagation();
+  }
+
+}
+
+Hooks.on('renderSceneControls', (controls, html) => {
+  console.log("HHD here", html);
+  HouseholdDice.Init(controls, html);
+});
+
+console.log("HHD | Household Dice Roller loaded");
+
+Handlebars.registerHelper('getImageForValue', function(value) {
+  const images = [
+    '/modules/household-dice/assets/house.webp',
+    '/modules/household-dice/assets/hearts.webp',
+    '/modules/household-dice/assets/diamonds.webp',
+    '/modules/household-dice/assets/clubs.webp',
+    '/modules/household-dice/assets/spades.webp',
+    '/modules/household-dice/assets/hold.webp'
+  ];
+
+  return images[value - 1];
+});
